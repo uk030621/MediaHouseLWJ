589a70b9f9d17b0d437ed0df14d27a962c3963bd
uk030621 <123797901+uk030621@users.noreply.github.com>
2025-01-19 22:11:27 +0000

    Added icon to view and delete buttons.

--- app/enhanced/page.js
+++ app/enhanced/page.js
@@ -210,21 +210,21 @@ export default function Home() {
       ) : (
         <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {filteredUrls.map((media) => (
-            <li key={media._id} className="p-4 border bg-slate-200 rounded-md">
-              <h3 className="text-lg font-semibold mb-2">
+            <li key={media._id} className="p-4 border bg-slate-100 rounded-md ">
+              <h3 className="text-sm text-black font-thin mb-2">
                 {decodeHtmlEntities(media.title)}
               </h3>
               <button
                 onClick={() => setSelectedMedia(media)}
-                className="bg-green-500 text-white px-4 py-2 rounded-md w-fit mr-10 text-sm"
+                className=" text-slate-700 px-4 py-2 rounded-md w-fit mr-10 text-xl"
               >
-                View
+                📺<span className="text-xs">View</span>
               </button>
               <button
                 onClick={() => handleDelete(media._id)}
-                className="bg-red-500 text-white px-4 py-2 rounded-md mt-2 w-fit text-sm"
+                className=" text-slate-700 px-4 py-2 rounded-md mt-2 w-fit text-xl"
               >
-                Delete
+                🗑️<span className="text-xs">Delete</span>
               </button>
             </li>
           ))}
