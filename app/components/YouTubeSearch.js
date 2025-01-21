2510853be693486ad131b8414cd4c86ab1b06547
uk030621 <123797901+uk030621@users.noreply.github.com>
2025-01-21 16:06:16 +0000

    commented out Link imports

--- app/components/YouTubeSearch.js
+++ app/components/YouTubeSearch.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
+//import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 const YouTubeSearch = () => {
--- app/customsearch/page.js
+++ app/customsearch/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Link from "next/link";
+//import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function SearchComponent() {
